862dea46dc5bc707ac759029e1491fee3f49265f
chocolate-kuma <natsumi0814okada@gmail.com>
2024-10-15 20:10:44 +0900

    Add client form

---
+++ ClientForm.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace LeftDeviceWindows
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button7 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            button10 = new Button();
+            buttonCopy = new Button();
+            buttonPaste = new Button();
+            buttonCut = new Button();
+            buttonScreenShot = new Button();
+            SuspendLayout();
+            // 
+            // button7
+            // 
+            button7.Location = new Point(263, 35);
+            button7.Name = "button7";
+            button7.Size = new Size(80, 80);
+            button7.TabIndex = 0;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(263, 207);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 80);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(349, 207);
+            button2.Name = "button2";
+            button2.Size = new Size(80, 80);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(435, 207);
+            button3.Name = "button3";
+            button3.Size = new Size(80, 80);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(263, 121);
+            button4.Name = "button4";
+            button4.Size = new Size(80, 80);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(349, 121);
+            button5.Name = "button5";
+            button5.Size = new Size(80, 80);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(435, 121);
+            button6.Name = "button6";
+            button6.Size = new Size(80, 80);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(349, 35);
+            button8.Name = "button8";
+            button8.Size = new Size(80, 80);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(435, 35);
+            button9.Name = "button9";
+            button9.Size = new Size(80, 80);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button0
+            // 
+            button0.Location = new Point(263, 293);
+            button0.Name = "button0";
+            button0.Size = new Size(80, 80);
+            button0.TabIndex = 9;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(349, 293);
+            button10.Name = "button10";
+            button10.Size = new Size(80, 80);
+            button10.TabIndex = 10;
+            button10.Text = ".";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // buttonCopy
+            // 
+            buttonCopy.Location = new Point(57, 35);
+            buttonCopy.Name = "buttonCopy";
+            buttonCopy.Size = new Size(200, 166);
+            buttonCopy.TabIndex = 11;
+            buttonCopy.Text = "Copy";
+            buttonCopy.UseVisualStyleBackColor = true;
+            buttonCopy.Click += buttonCopy_Click_1;
+            // 
+            // buttonPaste
+            // 
+            buttonPaste.Location = new Point(57, 207);
+            buttonPaste.Name = "buttonPaste";
+            buttonPaste.Size = new Size(200, 166);
+            buttonPaste.TabIndex = 12;
+            buttonPaste.Text = "Paste";
+            buttonPaste.UseVisualStyleBackColor = true;
+            buttonPaste.Click += buttonPaste_Click;
+            // 
+            // buttonCut
+            // 
+            buttonCut.Location = new Point(521, 35);
+            buttonCut.Name = "buttonCut";
+            buttonCut.Size = new Size(200, 166);
+            buttonCut.TabIndex = 13;
+            buttonCut.Text = "Cut";
+            buttonCut.UseVisualStyleBackColor = true;
+            buttonCut.Click += buttonCut_Click_1;
+            // 
+            // buttonScreenShot
+            // 
+            buttonScreenShot.Location = new Point(521, 207);
+            buttonScreenShot.Name = "buttonScreenShot";
+            buttonScreenShot.Size = new Size(200, 166);
+            buttonScreenShot.TabIndex = 14;
+            buttonScreenShot.Text = "ScreenShot";
+            buttonScreenShot.UseVisualStyleBackColor = true;
+            buttonScreenShot.Click += buttonScreenShot_Click;
+            // 
+            // ClientForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonScreenShot);
+            Controls.Add(buttonCut);
+            Controls.Add(buttonPaste);
+            Controls.Add(buttonCopy);
+            Controls.Add(button10);
+            Controls.Add(button0);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button7);
+            Name = "ClientForm";
+            Text = "ClientForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button7;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button button10;
+        private Button buttonCopy;
+        private Button buttonPaste;
+        private Button buttonCut;
+        private Button buttonScreenShot;
+    }
+}
---
+++ ClientForm.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//namespace LeftDeviceWindows
+//{
+//    public partial class ClientForm : Form
+//    {
+//        public ClientForm()
+//        {
+//            InitializeComponent();
+//        }
+
+
+
+//        private void buttonScreenShot_Click(object sender, EventArgs e)
+//        {
+
+//        }
+
+//        private void buttonCopy_Click_1(object sender, EventArgs e)
+//        {
+
+//        }
+
+//        private void buttonCut_Click_1(object sender, EventArgs e)
+//        {
+
+//        }
+//    }
+//}
+
+
+namespace LeftDeviceWindows
+{
+    public enum ActionType
+    {
+        Hotkey,
+        Typewrite,
+        // 他のアクションタイプがあれば追加
+    }
+
+    public class ActionEntity
+    {
+        public ActionType Type { get; set; }
+        public string Value { get; set; }
+    }
+
+    public partial class ClientForm : Form
+    {
+        private TextBox textBoxIpAddress;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+
+            //// フォームの初期設定
+            //this.Text = "クライアントウィンドウ";
+            //this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            //this.MaximizeBox = false;
+            //this.ClientSize = new System.Drawing.Size(1000, 600);
+
+            // コントロールを追加
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
+            // IPアドレス入力
+            Label labelIpAddress = new Label() { Text = "IPア:", Left = 10, Top = 10, Width = 70 };
+            textBoxIpAddress = new TextBox() { Left = 90, Top = 10, Width = 150 };
+
+            //// アクションタイプ選択
+            //Label labelActionType = new Label() { Text = "アクションタイプ:", Left = 10, Top = 40, Width = 70 };
+            //comboBoxActionType = new ComboBox() { Left = 90, Top = 40, Width = 150 };
+            //comboBoxActionType.Items.AddRange(Enum.GetNames(typeof(ActionType)));
+            //comboBoxActionType.SelectedIndex = 0;
+
+            //// 値入力
+            //Label labelValue = new Label() { Text = "値:", Left = 10, Top = 70, Width = 70 };
+            //textBoxValue = new TextBox() { Left = 90, Top = 70, Width = 150 };
+
+            //// 送信ボタン
+            //buttonSend = new Button() { Text = "送信", Left = 90, Top = 100, Width = 80 };
+            //buttonSend.Click += ButtonSend_Click;
+
+            // コントロールをフォームに追加
+            this.Controls.Add(labelIpAddress);
+            this.Controls.Add(textBoxIpAddress);
+        }
+
+        private async void ButtonSend_Click(String value, ActionType actionType)
+        {
+            string ipAddress = textBoxIpAddress.Text.Trim();
+
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                MessageBox.Show("IPアドレスを入力してください");
+                return;
+            }
+
+            
+            ActionEntity action = new ActionEntity { Type = actionType, Value = value };
+            await SendData(action, ipAddress);
+        }
+
+        private async Task SendData(ActionEntity action, string ipAddress)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = $"http://{ipAddress}:8080/data";
+
+                var data = new Dictionary<string, string>
+                {
+                    { "data", $"{action.Type.ToString().ToLower()}_+_{action.Value}" }
+                };
+
+                var json = JsonSerializer.Serialize(data);
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        MessageBox.Show("Success: " + responseBody);
+                    }
+                    else
+                    {
+                        MessageBox.Show("データの送信に失敗しました");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+        }
+
+        private void buttonScreenShot_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("win shift s", ActionType.Hotkey);
+        }
+
+        private void buttonCopy_Click_1(object sender, EventArgs e)
+        {
+            ButtonSend_Click("ctrl c", ActionType.Hotkey);
+        }
+
+        private void buttonCut_Click_1(object sender, EventArgs e)
+        {
+            ButtonSend_Click("ctrl x", ActionType.Hotkey);
+        }
+
+        private void buttonPaste_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("ctrl v", ActionType.Hotkey);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("1", ActionType.Typewrite);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("2", ActionType.Typewrite);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("3", ActionType.Typewrite);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("4", ActionType.Typewrite);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("5", ActionType.Typewrite);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("6", ActionType.Typewrite);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("7", ActionType.Typewrite);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("8", ActionType.Typewrite);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("9", ActionType.Typewrite);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ButtonSend_Click("0", ActionType.Typewrite);
+        }
+    }
+}
